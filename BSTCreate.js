3057669316715a5a5af108115c5f24204d187fec
Jin Kang <36521896+DioDoesNotCare@users.noreply.github.com>
2023-07-25 01:55:16 -0500

    Add files via upload

---
+++ BSTCreate.js
@@ -0,0 +1,49 @@
+class BinarySearchTree 
+{
+    constructor() {
+        this.root = null;
+    }
+    
+    insert(data) {
+        var newNode = new Node(data);
+        
+        if (this.root == null) {
+            root = newNode;
+        }
+        else {
+            this.insertNode(this.root, newNode);
+        }
+
+    }
+
+    insertNode(node, newNode) {
+
+        if (newNode.data < node.data) {
+            if (node.left == null) {
+                node.left = newNode;
+            }
+            else {
+                insertNode(node.left, newNode);
+            }
+        }
+
+        else {
+
+            if (node.right == null) {
+                node.right = newNode;
+            }
+
+            else {
+                insertNode(node.right, newNode);
+            }
+        }
+    }
+}
+
+class Node {
+    constructor(data) {
+        this.data = data; 
+        this.left = null;
+        this.right = null;
+    }
+}
