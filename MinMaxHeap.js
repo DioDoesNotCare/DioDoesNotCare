b2ddb8a424ac36d1b3c261c24edffbcc3cbe4c76
Jin Kang <36521896+DioDoesNotCare@users.noreply.github.com>
2023-08-02 04:01:53 -0500

    Update MinMaxHeap.js

--- MinMaxHeap.js
+++ MinMaxHeap.js
@@ -10,7 +10,7 @@ class MinHeap {
         }
 
         else {
-            const minNode = this.heap[0];
+            let minNode = this.heap[0];
             const lastNode = this.heap.pop();
 
             if (this.heap.length > 0) {
@@ -24,14 +24,26 @@ class MinHeap {
 
     insert(node) {
         this.heap.push(node);
-        this.bubbleUp();
+        this.bubbleUp(this.heap.length - 1);
     }
 
-    bubbleUp() {
-
+    bubbleUp(index) {
+        if (index == 0) {
+            return;
+        }
+        while (this.heap[index] < this.heap[Math.floor((index - 1) / 2)]) {
+            var parentNode = this.heap[Math.floor((index - 1) / 2)];
+            var temp = parentNode
+            console.log(this.heap[index]);
+            this.heap[Math.floor((index - 1) / 2)] = this.heap[index];
+            this.heap[index] = temp;
+            this.bubbleUp(Math.floor((index - 1) / 2))
+        }
     }
 
-    bubbleDown() {
-        
-    }
 }
+
+var min = new MinHeap();
+min.insert(10);
+min.insert(5);
+console.log(min.heap);
