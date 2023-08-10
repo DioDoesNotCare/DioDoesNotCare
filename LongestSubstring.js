4af99b2f68082f6e4042e2efc23fef18ec024915
Jin Kang <36521896+DioDoesNotCare@users.noreply.github.com>
2023-08-10 18:05:02 -0500

    Update LongestSubstring.js

--- LongestSubstring.js
+++ LongestSubstring.js
@@ -1,27 +1,60 @@
-function longestSubstring(s) {
-    const set = new Set();
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
 
-    let leftPointer = 0;
-    let rightPointer = 0;
-    let highestLength = 0;
+    let longestSubstring = "";
+    let isEven = false;
 
-    while (rightPointer != s.length) {
-        if (!set.has(s[rightPointer])) {
-            set.add(s[rightPointer]);
-            rightPointer++;
-            if (set.size > highestLength) {
-                highestLength = set.size;
-            }
+    if (s.length == 1) {
+        return s;
+    }
+
+    if (s.length == 2) { 
+        if (s[0] == s[1]) {
+            return s;
         }
         else {
-            set.delete(s[leftPointer]);
-            leftPointer++;
+            return s[0];
         }
     }
 
-    console.log(highestLength);
+    if (s.length % 2 == 0) {
+        isEven = true;
+        let leftHalf = s.slice(0, s.length / 2);
+        let rightHalf = s.slice(s.length / 2, s.length);
+        s = leftHalf + "." + rightHalf;
+    }
 
-}
+    for (let i = 1; i < s.length - 1 ; i++) {
+        let substring = "";
+        let j = 1;
+
+        
+
+        while (i - j != -1 && i + j != s.length) {
+            
+            if (s[i - j] == s[i + j]) {
+
+                if (substring.length == 0) {
+                    if (s[i] != ".") {
+                        substring = s[i];
+                    }
+                }
+                
+                substring = s[i - j] + substring + s[i + j];
+            }
+
+            j++;
+        }
 
 
-longestSubstring("abccabazx");
+        if (substring.length > longestSubstring.length) {
+            longestSubstring = substring;
+        }
+        
+    }
+
+    return longestSubstring;
+};
